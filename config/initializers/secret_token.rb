01949071a3eb1654633c0a099f09b64862842cb3
Lucas Souza <lucasas@gmail.com>
2010-08-17 14:45:28 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Programadorpoliglota::Application.config.secret_token = '5d47f2d83458cf525317b9029ba2cd09ef10ca8a0dddf88cb57af35e126dfc5ade030a9fb1b96933d2d89ccde5631c40d7249851d550e23045fed8f9ac55cac6'
