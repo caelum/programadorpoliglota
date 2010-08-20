777b380490e9a51ddbeffe7282375cbeb30f3e0a
Adriano Henrique de Almeida <adrianoalmeida7@gmail.com>
2010-08-20 12:41:51 -0300

    test fixed

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -4,12 +4,17 @@ describe TweetsController do
   describe "#index" do
     it "should retrive all tweets for #java Tag" do
       tweets = [Tweet.new, Tweet.new]
-      join = Object.new
-      Tweet.should_receive(:joins).with(:tag).and_return(join) 
-      join.should_receive(:where).with(:tags=>{:name=>'#java'}).and_return(tweets)
+      tags = [Tag.new(:name=>'#java')]
+      
+      Tag.should_receive(:all).and_return(tags)
+      query = Object.new
+      Tweet.should_receive(:joins).with(:tag).and_return(query) 
+      query.should_receive(:where).with(:tags=>{:name=>'#java'}).and_return(query)
+      query.should_receive(:order).with('date DESC').and_return(tweets)
       
       get :index
-      assigns(:tweets).should eq(tweets)
+      tweets_found = assigns(:tweets)
+      tweets_found['#java'].should eq(tweets)
     end
   end
 end
