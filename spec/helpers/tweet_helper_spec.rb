d16476c23522667fd0469370cea4ca899db27363
Lucas Souza <lucasas@gmail.com>
2010-08-31 16:58:49 -0300

    comentarios inuteis removidos

--- spec/helpers/tweet_helper_spec.rb
+++ spec/helpers/tweet_helper_spec.rb
@@ -1,15 +1,5 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TweetHelper. For example:
-#
-# describe TweetHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe TweetHelper do
   describe '#next_page_link' do
     it 'should return the next page link' do
