1e07e72442b464cc550066503c2a0caf3b3d75e3
Lucas Souza <lucasas@gmail.com>
2010-08-31 16:47:10 -0300

    refatoracao na tela e imagem default pra twitters desconhecidos

--- spec/helpers/tweet_helper_spec.rb
+++ spec/helpers/tweet_helper_spec.rb
@@ -22,4 +22,16 @@ describe TweetHelper do
       helper.previous_page_link(5, 3).should == '<a href="http://test.host/tweets/see_more/3/4" class="ver_mais">Anteriores</a>'
     end
   end
+  
+  describe '#image_url_of' do
+    it 'should return the image url of the given user when it has one' do
+      user = User.new :image_url => 'url'
+      helper.image_url_of(user).should == 'url'
+    end
+    
+    it 'should return the default image url of the given user when does not have one' do
+      user = User.new
+      helper.image_url_of(user).should == '/images/default_twitter.png'
+    end
+  end
 end
