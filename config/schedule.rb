d800f87d36e160a28780230ee8746f3d9578723e
Adriano Henrique de Almeida <adrianoalmeida7@gmail.com>
2010-09-22 15:44:18 -0300

    larger schedule interval (not dummy anymore)

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 job_type :runner,  'cd :path && rails runner -e :environment ":task"'
 env "GEM_HOME", ENV["GEM_HOME"]
 
-every 1.minute do
+every 5.minute do
   
   runner "Tweet.add_new_tweets"
   
