1c1308a8338d173a320cdeb0fddad10c593ec13a
Adriano Henrique de Almeida <adrianoalmeida7@gmail.com>
2010-09-25 22:29:44 -0300

    colocando seeds pra popular inicial o bd

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+java   = TagGroup.create(:name=>'Java')
+ruby   = TagGroup.create(:name=>'Ruby')
+python = TagGroup.create(:name=>'Python')
+dotnet = TagGroup.create(:name=>'.NET')
+php    = TagGroup.create(:name=>'PHP')
+agile  = TagGroup.create(:name=>'Agile')
+
+java.tags << Tag.create(:name=>'#java')
+java.tags << Tag.create(:name=>'#vraptor')
+java.tags << Tag.create(:name=>'#jsf')
+java.tags << Tag.create(:name=>'#wicket')
+java.tags << Tag.create(:name=>'#struts')
+java.tags << Tag.create(:name=>'#android')
+
+ruby.tags << Tag.create(:name=>'#ruby')
+ruby.tags << Tag.create(:name=>'#rails')
+ruby.tags << Tag.create(:name=>'#sinatra')
+ruby.tags << Tag.create(:name=>'#merb')
+
+python.tags << Tag.create(:name=>'#python')
+python.tags << Tag.create(:name=>'#django')
+python.tags << Tag.create(:name=>'#web2py')
+
+dotnet.tags << Tag.create(:name=>'#dotnet')
+dotnet.tags << Tag.create(:name=>'#csharp')
+dotnet.tags << Tag.create(:name=>'#linq')
+
+php.tags << Tag.create(:name=>'#php')
+
+agile.tags << Tag.create(:name=>'#agile')
+agile.tags << Tag.create(:name=>'#scrum')
+agile.tags << Tag.create(:name=>'#kanban')
+agile.tags << Tag.create(:name=>'#lean')
