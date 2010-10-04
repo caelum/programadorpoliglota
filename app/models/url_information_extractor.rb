a8295558a448cb957186f6296d1b936bd35a382b
Adriano Henrique de Almeida <adrianoalmeida7@gmail.com>
2010-10-04 14:06:30 -0300

    removendo espacos

--- app/models/url_information_extractor.rb
+++ app/models/url_information_extractor.rb
@@ -20,7 +20,7 @@ class URLInformationExtractor
       agent.user_agent_alias = 'Mac Safari'
       agent.get(@url).title
     rescue Exception => e
-      logger   = Logger.new('links.log')
+      logger = Logger.new('links.log')
       logger.error "Problems extracting title: #{e}. Will use the full url as the title"
       @url
     end
