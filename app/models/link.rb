855bc31c392f2fb7ba4bb203a8f018f636383112
Adriano Henrique de Almeida <adrianoalmeida7@gmail.com>
2010-10-04 12:04:55 -0300

    adicionando limit na busca dos links e tirando espacos inuteis

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,30 +1,28 @@
 class Link < ActiveRecord::Base
   belongs_to :tag_group
   
-  def self.create_from (tweet)
+  def self.create_from(tweet)
     urls = scan_for_urls(tweet.text)
     logger.debug "Found #{urls.size} urls for the tweet: #{tweet.id}"
     urls.each do |url|
-        extractor = URLInformationExtractor.new url
-        logger.debug "The url at the tweet is: #{url} ... preparing to unwrap it"
-        
-        full_url = extractor.unwrap
-        logger.debug "The full url found is: #{full_url}"
-        title = extractor.title
-        logger.debug "The title of url found is: #{title}"
-        
-        link = Link.create :url=>full_url, :tag_group=>tweet.tag_group, :title=>title
-        tweet.tag_group.links << link
+      extractor = URLInformationExtractor.new url
+      logger.debug "The url at the tweet is: #{url} ... preparing to unwrap it"
+
+      full_url = extractor.unwrap
+      logger.debug "The full url found is: #{full_url}"
+      title = extractor.title
+      logger.debug "The title of url found is: #{title}"
+
+      link = Link.create :url=>full_url, :tag_group=>tweet.tag_group, :title=>title
+      tweet.tag_group.links << link
     end
   end
   
   def self.most_popular_for(tag_group)
-      Link.select("count(*) as quantidade, url, title").where(["created_at > ? AND tag_group_id = ?", 7.days.ago, tag_group.id]).group("url").order("quantidade desc").all
+      Link.select("count(*) as quantidade, url, title").where(["created_at > ? AND tag_group_id = ?", 7.days.ago, tag_group.id]).group("url").order("quantidade desc").limit(3).all
   end
 
   def self.scan_for_urls(text)
     text.scan(/http[s]?:\/\/+[\w\d:\#\@\%\/;\$\(\)\~\_\?\+\-\=\\\.&]+/)
   end  
 end
-
-
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,18 +5,18 @@ describe Link do
   describe "#scan_for_urls" do
     it "should ignore link when it is just http:// (incomplete)" do
       text = "Esse eh um teste http:// com link incompleto"
-      Link.scan_for_urls(text).size.should == 0      
+      Link.scan_for_urls(text).size.should == 0
     end
 
     it "should work with https" do
       text = "Esse eh um teste https://secure.com/a"
       Link.scan_for_urls(text)[0].should == "https://secure.com/a"
-    end    
+    end
 
     it "should work with trailing marks: !" do
       text = "Esse eh um teste http://url.com/a!!!"
       Link.scan_for_urls(text)[0].should == "http://url.com/a"
-    end    
+    end
 
     it "should work with trailing marks: . must be accepted, unfortunately" do
       text = "Esse eh um teste http://url.com/a. bla bla"
@@ -28,7 +28,7 @@ describe Link do
     it "should extract all links and save then for a given tweet" do
       tag_java = TagGroup.new :name=>"#java"
       tweet = Tweet.new :text=>"Esse eh um teste com http://bit.ly/a1 e http://bit.ly/a2 #java", :tag_group => tag_java
-      
+
       extractor = stub(URLInformationExtractor)
       URLInformationExtractor.should_receive(:new).with('http://bit.ly/a1').and_return(extractor)
       URLInformationExtractor.should_receive(:new).with('http://bit.ly/a2').and_return(extractor)
@@ -36,40 +36,39 @@ describe Link do
       extractor.should_receive(:unwrap).and_return('http://blog.caelum.com.br')
       extractor.should_receive(:title).and_return('Caelum')
       extractor.should_receive(:title).and_return('Blog da Caelum')
-          
+
       Link.create_from tweet
       Link.all.size.should == 2
     end
   end
-  
+
   describe "#most_popular_for" do
     it "should return most popular links for a given tag ordered by quantity" do
       tag = TagGroup.create :name=>'#java'
-      
+
       Link.create :url=>'http://www.caelum.com.br', :tag_group=>tag, :created_at => Time.now - 4.day
       Link.create :url=>'http://www.caelum.com.br', :tag_group=>tag, :created_at => Time.now - 5.day
       Link.create :url=>'http://www.caelum.com.br', :tag_group=>tag, :created_at => Time.now - 6.day
       Link.create :url=>'http://www.caelum.com.br', :tag_group=>tag, :created_at => Time.now - 15.day
-      
+
       Link.create :url=>'http://blog.caelum.com.br', :tag_group=>tag, :created_at => Time.now - 2.day
       Link.create :url=>'http://blog.caelum.com.br', :tag_group=>tag, :created_at => Time.now - 5.day
-      
+
       Link.create :url=>'http://www.sun.com/java', :tag_group=>tag, :created_at => Time.now - 20.day
-      
+
       popular_links = Link.most_popular_for(tag)
-      
+
       popular_links.size.should == 2
-      popular_links.first.url.should == 'http://www.caelum.com.br'
-      popular_links.last.url.should == 'http://blog.caelum.com.br'
+      popular_links[0].url.should == 'http://www.caelum.com.br'
+      popular_links[1].url.should == 'http://blog.caelum.com.br'
     end
-    
+
     it "should not return a link if it was tweeted more than seven days" do
       tag = TagGroup.create :name=>'#java'
       Link.create :url=>'http://www.sun.com/java', :tag_group=>tag, :created_at => Time.now - 8.day
-      
+
       popular_links = Link.most_popular_for(tag)
       popular_links.size.should == 0
     end
-  end  
-  
+  end
 end
