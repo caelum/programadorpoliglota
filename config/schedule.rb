623648e6e763938dde95cb8d91633f6bc3dbc2a0
Adriano Henrique de Almeida <adrianoalmeida7@gmail.com>
2010-10-05 11:34:52 -0300

    atualizando documentação e o schedule pra job certa de import

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,5 @@ job_type :runner,  'cd :path && rails runner -e :environment ":task"'
 env "GEM_HOME", ENV["GEM_HOME"]
 
 every 5.minute do
-  
-  runner "Tweet.add_new_tweets"
-  
+  runner "TweetsRetrievalJob.new.import_all_groups_tweets"
 end
